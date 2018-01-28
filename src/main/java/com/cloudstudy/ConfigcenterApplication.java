4314b9baa8da46cb4ddb0f22f74935adf05c8152
sunlei <365243317@qq.com>
2018-01-28 17:30:24 +0800

    fix

--- src/main/java/com/cloudstudy/ConfigcenterApplication.java
+++ src/main/java/com/cloudstudy/ConfigcenterApplication.java
@@ -1,11 +1,11 @@
 package com.cloudstudy;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
-@EnableAutoConfiguration
+@EnableConfigServer
 public class ConfigcenterApplication {
 
 	public static void main(String[] args) {
