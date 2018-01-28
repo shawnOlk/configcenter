ab3e14eed679c82bec55c51ee0a98b43ba46f3a9
sunlei <365243317@qq.com>
2018-01-28 17:06:59 +0800

    config

---
+++ src/main/java/com/cloudstudy/ConfigcenterApplication.java
@@ -0,0 +1,14 @@
+package com.cloudstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@EnableAutoConfiguration
+public class ConfigcenterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigcenterApplication.class, args);
+	}
+}
